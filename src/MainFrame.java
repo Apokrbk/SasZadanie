a5be96bfc38889a1c6d27c5022407783a0dc0e87
Apokrbk <damian.portasinski@gmail.com>
2017-05-13 20:39:22 +0200

    Refactor

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -16,25 +16,11 @@ public class MainFrame extends JFrame{
     public MainFrame()
     {
         super("Hello");
-        setSize(900,300);
-        setResizable(false);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        initFrame();
+        JLabel labels[] = new JLabel[7];
+        initLabels(labels);
         Container container = this.getContentPane();
-
-        setLayout(new GridLayout(10,1));
-
-        JLabel jLabel5 = new JLabel("Podaj wspolczynniki oddzielając je spacją w kolejności: poczatek przedzialu, koniec przedzialu" +
-                ", dokładność, maksymalna liczba iteracji, wspolczynniki funkcji.");
-        JLabel jLabel = new JLabel("Wybrana funkcja:             ");
-        JLabel jLabel1 = new JLabel("Zadany przedział:          ");
-        JLabel jLabel3 = new JLabel("Dokładność obliczonej wartości pierwiastka:          ");
-        JLabel jLabel4 = new JLabel("Maksymalna liczba iteracji:          ");
-        JLabel jLabel2 = new JLabel("Miejsce zerowe wybranej funkcji:             ");
-        JLabel jLabel6 = new JLabel("Komunikat: ");
-        TextArea textArea = new TextArea();
-        textArea.setEditable(true);
-        textArea.setFont(new Font("Serif", Font.PLAIN, 20));
+        TextArea textArea = initTextArea();
         JButton button = new JButton("Wybierz plik z danymi");
         JButton button1 = new JButton("Użyj wpisanych danych");
         button1.addActionListener(new ActionListener() {
@@ -51,17 +37,17 @@ public class MainFrame extends JFrame{
 
                 try{
                     RootValueFinder rootValueFinder = new RootValueFinder(data);
-                    jLabel.setText("Wybrana funkcja: "+ rootValueFinder.getPolynomialFunction().toString());
-                    jLabel1.setText("Zadany przedział: " + rootValueFinder.getRange());
-                    jLabel3.setText("Dokładność obliczanej wartości pierwiastka: " + rootValueFinder.getEps());
-                    jLabel4.setText("Maksymalna liczba iteracji: " + rootValueFinder.getMax());
-                    jLabel2.setText("Miejsce zerowe wybranej funkcji to: " + rootValueFinder.findRootValueOfFunction());
-                    jLabel6.setText("Komunikat: Miejsce zerowe znalezione z odpowiednią dokładnością");
+                    labels[1].setText("Wybrana funkcja: "+ rootValueFinder.getPolynomialFunction().toString());
+                    labels[2].setText("Zadany przedział: " + rootValueFinder.getRange());
+                    labels[3].setText("Dokładność obliczanej wartości pierwiastka: " + rootValueFinder.getEps());
+                    labels[4].setText("Maksymalna liczba iteracji: " + rootValueFinder.getMax());
+                    labels[5].setText("Miejsce zerowe wybranej funkcji to: " + rootValueFinder.findRootValueOfFunction());
+                    labels[6].setText("Komunikat: Miejsce zerowe znalezione z odpowiednią dokładnością");
                 }
                 catch(Exception exception)
                 {
-                    jLabel6.setText("Komunikat: "+ exception.toString());
-                    jLabel2.setText("Miejsce zerowe wybranej funkcji to: NIE ZNALEZIONO");
+                    labels[6].setText("Komunikat: "+ exception.toString());
+                    labels[5].setText("Miejsce zerowe wybranej funkcji to: NIE ZNALEZIONO");
                 }
 
             }
@@ -73,37 +59,63 @@ public class MainFrame extends JFrame{
                 jFileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
                 jFileChooser.showSaveDialog(MainFrame.this);
                 String filename = jFileChooser.getSelectedFile().getAbsolutePath();
-                Scanner input = new Scanner(System.in);
                 FileReader fileReader = new FileReader(filename);
+                textArea.setText("");
                 try{
                     RootValueFinder rootValueFinder = new RootValueFinder(fileReader.getData());
-                    jLabel.setText("Wybrana funkcja: "+ rootValueFinder.getPolynomialFunction().toString());
-                    jLabel1.setText("Zadany przedział: " + rootValueFinder.getRange());
-                    jLabel3.setText("Dokładność obliczanej wartości pierwiastka: " + rootValueFinder.getEps());
-                    jLabel4.setText("Maksymalna liczba iteracji: " + rootValueFinder.getMax());
-                    jLabel2.setText("Miejsce zerowe wybranej funkcji to: " + rootValueFinder.findRootValueOfFunction());
-                    jLabel6.setText("Komunikat: Miejsce zerowe znalezione z odpowiednią dokładnością");
+                    labels[1].setText("Wybrana funkcja: "+ rootValueFinder.getPolynomialFunction().toString());
+                    labels[2].setText("Zadany przedział: " + rootValueFinder.getRange());
+                    labels[3].setText("Dokładność obliczanej wartości pierwiastka: " + rootValueFinder.getEps());
+                    labels[4].setText("Maksymalna liczba iteracji: " + rootValueFinder.getMax());
+                    labels[5].setText("Miejsce zerowe wybranej funkcji to: " + rootValueFinder.findRootValueOfFunction());
+                    labels[6].setText("Komunikat: Miejsce zerowe znalezione z odpowiednią dokładnością");
                 }
                 catch(Exception exception)
                 {
-                    jLabel6.setText("Komunikat: "+ exception.toString());
-                    jLabel2.setText("Miejsce zerowe wybranej funkcji to: NIE ZNALEZIONO");
+                    labels[6].setText("Komunikat: "+ exception.toString());
+                    labels[5].setText("Miejsce zerowe wybranej funkcji to: NIE ZNALEZIONO");
                 }
 
             }
         });
         button.setSize(100,70);
-      container.add(button);
-      container.add(jLabel5);
-      container.add(textArea);
-      container.add(button1);
-        container.add(jLabel);
-        container.add(jLabel1);
-        container.add(jLabel4);
-        container.add(jLabel3);
-        container.add(jLabel2);
-        container.add(jLabel6);
+        addToContainer(labels, container, textArea, button, button1);
         setVisible(true);
 
+    }
+
+    private void addToContainer(JLabel[] labels, Container container, TextArea textArea, JButton button, JButton button1) {
+        container.add(button);
+        container.add(labels[0]);
+        container.add(textArea);
+        container.add(button1);
+        for(int i=1; i<labels.length; i++)
+            container.add(labels[i]);
+    }
+
+    private TextArea initTextArea() {
+        TextArea textArea;
+        textArea = new TextArea();
+        textArea.setEditable(true);
+        textArea.setFont(new Font("Serif", Font.PLAIN, 20));
+        return textArea;
+    }
+
+    private void initFrame() {
+        setSize(900,300);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(10,1));
+    }
+
+    private void initLabels(JLabel[] labels) {
+        labels[0] = new JLabel("Podaj wspolczynniki oddzielając je spacją w kolejności: poczatek przedzialu, koniec przedzialu" +
+                ", dokładność, maksymalna liczba iteracji, wspolczynniki funkcji.");
+        labels[1] = new JLabel("Wybrana funkcja: ");
+        labels[2] = new JLabel("Zadany przedział: ");
+        labels[3] = new JLabel("Dokładność obliczanej wartości pierwiastka: ");
+        labels[4] = new JLabel("Maksymalna liczba iteracji: ");
+        labels[5] = new JLabel("Miejsce zerowe wybranej funkcji: ");
+        labels[6] = new JLabel("Komunikat: ");
     }
 }
